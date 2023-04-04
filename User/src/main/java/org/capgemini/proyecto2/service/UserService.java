3591ad244ee77c5596d348405741d884a95ba03b
Mesa Núñez <ayoze.mesa-nunez@capgemini.com>
2023-04-04 08:48:25 +0200

    Creado el endpoint para la creación de usuarios

--- User/src/main/java/org/capgemini/proyecto2/service/UserService.java
+++ User/src/main/java/org/capgemini/proyecto2/service/UserService.java
@@ -6,6 +6,17 @@ import org.capgemini.proyecto2.model.User;
 
 public interface UserService {
 
+	/**
+	 * Devuelve la lista de usuarios disponible en BBDD
+	 * @return
+	 */
 	public List<User> findAll();
+	
+	/**
+	 * Guarda un usuario en BBDD
+	 * @param user
+	 * @return
+	 */
+	public User save(User user);
 
 }
