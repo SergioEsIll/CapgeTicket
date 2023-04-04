28f69ba941f70db7f1a23e54201b9584953daa8f
Llorente Castelblanque <emiliano-javier.llorente-castelblanque@capgemini.com>
2023-04-04 20:55:18 +0200

    Añadida clase OpenApiConfig en Purchase para documentación de la API

---
+++ Purchase/src/main/java/org/capgemini/proyecto2/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package org.capgemini.proyecto2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI EventOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Purchase API").description("Documentación de Purchase API").version("v1.0")
+						.contact(new Contact().name("Grupo 4"))
+						.license(new License().name("LICENSE").url("http://springdoc.org")))
+				.externalDocs(new ExternalDocumentation().description("Descripción del proyecto")
+						.url("https://miproyecto.es"));
+
+	}
+
+}
