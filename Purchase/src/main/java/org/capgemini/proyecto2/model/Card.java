ffd29193778165519654c25616a6fad83283c389
Mesa Núñez <ayoze.mesa-nunez@capgemini.com>
2023-04-05 13:05:31 +0200

    Validaciones para los campos de la entidad card añadida

--- Purchase/src/main/java/org/capgemini/proyecto2/model/Card.java
+++ Purchase/src/main/java/org/capgemini/proyecto2/model/Card.java
@@ -1,14 +1,41 @@
 package org.capgemini.proyecto2.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 public class Card {
 
+	@NotNull @NotEmpty(message = "El nombre de titular no puede estar vacío")
+	@Size(min = 3, max = 40)
 	private String nombreTitular;
+	
+	@NotNull @NotEmpty(message = "El formato para Visa es: 4aaa-bbbb-cccc-dddd / 4aaabbbbccccdddd /n"
+			+ "El formato para Mastercard es: 5xaa-bbbb-cccc-dddd / 5xaabbbbccccdddd   donde x es un valor entre 1-5")
+	@Size(min = 16, max = 16)
+	@Positive
 	private long numeroTarjeta;
+	
+	@NotNull @NotEmpty(message = "¡El mes no puede tener un '0' delante! y tiene que estar entre 1 y 12")
+	@Size(min = 1, max = 2) @Min(1) @Max(12)
 	private int mesCaducidad;
+	
+	@NotNull @NotEmpty(message = "¡El año no puede ser del pasado!")
+	@Size(min = 4, max = 4) @Min(2023) @Max(2099)
 	private int yearCaducidad;
+	
+	@NotNull @NotEmpty(message = "¡El CVV lo conforman 3 ó 4 dígitos!")
+	@Size(min = 3, max = 4) @Min(2023) @Max(2099)
 	private int cvv;
+	
+	
 	private String emisor;
 	private String concepto;
+	
+	@Positive @Max(10)
 	private int cantidad;
 
 	public Card() {
