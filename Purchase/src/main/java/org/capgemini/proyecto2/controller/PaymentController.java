c458518df973d854fae85c65c8fbaeb5318934fd
Mesa Núñez <ayoze.mesa-nunez@capgemini.com>
2023-04-05 13:08:38 +0200

    Control de respuesta desde la API FeignClient añadido

--- Purchase/src/main/java/org/capgemini/proyecto2/controller/PaymentController.java
+++ Purchase/src/main/java/org/capgemini/proyecto2/controller/PaymentController.java
@@ -1,9 +1,12 @@
 package org.capgemini.proyecto2.controller;
 
 import org.capgemini.proyecto2.model.Card;
+import org.capgemini.proyecto2.model.Ticket;
 import org.capgemini.proyecto2.response.PaymentResponse;
 import org.capgemini.proyecto2.service.PaymentService;
+import org.capgemini.proyecto2.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,16 +23,44 @@ public class PaymentController {
 	@Autowired
 	PaymentService paymentService;
 
+	@Autowired
+	TicketService ticketService;
+
 	@PostMapping
 	public ResponseEntity<PaymentResponse> payTicket(@RequestBody Card card) {
 
 		PaymentResponse result = paymentService.payTicket(card);
 
-		if (result == null) {
-			return ResponseEntity.notFound().build();
+		result.setTimestamp(null);
+
+		switch (result.getStatus()) {
+		case "200.0001": {
+			result.setStatus("200");
+
+			Ticket ticket = new Ticket();
+			ticket.setName(result.getInfo().getNombreTitular());
+			ticket.setPrice(result.getInfo().getCantidad());
+			ticketService.save(ticket);
+
+			result.setInfoAdicional(null);
+
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		}
+		case "400.0001":
+		case "400.0002":
+		case "400.0003": {
+			result.setInfoAdicional(null);
+			result.setStatus("400");
+			return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}
+		case "500.0001": {
+			result.setStatus("500");
+			return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		default:
+			return ResponseEntity.ok(result);
 		}
 
-		return ResponseEntity.ok(result);
 	}
 
 }
--- Purchase/src/main/java/org/capgemini/proyecto2/response/PaymentResponse.java
+++ Purchase/src/main/java/org/capgemini/proyecto2/response/PaymentResponse.java
@@ -3,28 +3,35 @@ package org.capgemini.proyecto2.response;
 import java.io.Serializable;
 import java.util.List;
 
+import org.capgemini.proyecto2.model.Card;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PaymentResponse implements Serializable {
 
 	/**
-	 * 
+	 * Default Serial Version
 	 */
 	private static final long serialVersionUID = 1L;
 	private String timestamp;
 	private String status;
 	private String error;
 	private List<String> message;
+	private Card info;
 	private String infoAdicional;
 
 	public PaymentResponse() {
 		super();
 	}
 
-	public PaymentResponse(String timestamp, String status, String error, List<String> message, String infoAdicional) {
+	public PaymentResponse(String timestamp, String status, String error, List<String> message, Card info, String infoAdicional) {
 		super();
 		this.timestamp = timestamp;
 		this.status = status;
 		this.error = error;
 		this.message = message;
+		this.setInfo(info);
 		this.infoAdicional = infoAdicional;
 	}
 
@@ -76,6 +83,14 @@ public class PaymentResponse implements Serializable {
 	public String toString() {
 		return "PaymentResponse [timestamp=" + timestamp + ", status=" + status + ", error=" + error + ", message="
 				+ message + ", infoAdicional=" + infoAdicional + "]";
+	}
+
+	public Card getInfo() {
+		return info;
+	}
+
+	public void setInfo(Card info) {
+		this.info = info;
 	}
 
 }
