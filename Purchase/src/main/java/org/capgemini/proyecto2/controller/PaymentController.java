cab58daa85f394a8a1c0344fc3dcccdc482fe709
Mesa Núñez <ayoze.mesa-nunez@capgemini.com>
2023-04-05 10:04:55 +0200

    Anotación @RestController añadida al controlador PaymentController

--- Purchase/src/main/java/org/capgemini/proyecto2/controller/PaymentController.java
+++ Purchase/src/main/java/org/capgemini/proyecto2/controller/PaymentController.java
@@ -5,12 +5,12 @@ import org.capgemini.proyecto2.response.PaymentResponse;
 import org.capgemini.proyecto2.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/payments")
 public class PaymentController {
 
